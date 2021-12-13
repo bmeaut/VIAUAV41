83926f88e2c012ba8cd4c19f4ccad1ca70c0eb50
Márton Braun <braunmarci@gmail.com>
2021-12-13 16:29:43 +0100

    Fix alphabet typo

--- projects/chapters/chapter-4/src/main/kotlin/com/example/something/TextUtils.kt
+++ projects/chapters/chapter-4/src/main/kotlin/com/example/something/TextUtils.kt
@@ -2,7 +2,7 @@
 
 package util
 
-val LOWERCASE_ALPHABET = "abcdefghijlkmnopqrstuvwxyz"
+val LOWERCASE_ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
 fun isEmpty(str: String?): Boolean {
     return str == null || str.length == 0
