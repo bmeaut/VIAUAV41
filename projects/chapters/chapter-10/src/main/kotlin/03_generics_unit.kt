0ea51328764eabb91b460c7535f2146d581f3113
Márton Braun <braunmarci@gmail.com>
2022-08-20 16:13:52 +0200

    Fix formatting

--- projects/chapters/chapter-10/src/main/kotlin/03_generics_unit.kt
+++ projects/chapters/chapter-10/src/main/kotlin/03_generics_unit.kt
@@ -1,17 +1,16 @@
-
 interface Task<T> {
     fun execute(): T
 }
 
 fun main() {
-    val stringResultTask = object: Task<String> {
+    val stringResultTask = object : Task<String> {
         override fun execute(): String {
             return "This is a result string"
         }
     }
-    val str = stringResultTask.execute()
+    val str: String = stringResultTask.execute()
 
-    val noResultTask = object: Task<Unit> {
+    val noResultTask = object : Task<Unit> {
         override fun execute() {
             println("Do something!")
         }
