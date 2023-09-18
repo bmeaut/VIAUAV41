52ff11728a84e8bbed12ce3078d84837fa5101ff
Márton Braun <braunmarci@gmail.com>
2023-09-18 22:32:34 +0200

    Update year

--- projects/chapters/chapter-2/src/main/kotlin/04_ctors.kt
+++ projects/chapters/chapter-2/src/main/kotlin/04_ctors.kt
@@ -1,5 +1,5 @@
 //region Helpers
-fun getCurrentYear() = 2022 // TODO update this implementation next year
+fun getCurrentYear() = 2023 // TODO update this implementation next year
 //endregion
 
 class Car(val model: String, val year: Int) {
